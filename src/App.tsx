69f6901b6d5fe096f4d79b218a538f161786a081
Jen <6406037+usrrname@users.noreply.github.com>
2021-12-06 12:11:37 -0500

    Decouple size from heading tag

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,55 @@ import { Heading, Text } from "./components/typography";
 function App() {
   return (
     <div className="App">
-      <Heading as="h1">Display 1</Heading>
-      <Heading as="h2">Display 2</Heading>
-      <Heading as="h3">Display 2</Heading>
-      <Heading as="h4">Display 4</Heading>
-      <Heading as="h5">Display 5</Heading>
-      <Heading as="h6">Display 6</Heading>
+      <section>
+        <Heading as="h1">Browser default size with H1 tag</Heading>
+        <Heading as="h2">Browser default size H2 tag</Heading>
+        <Heading as="h3">Browser default size H3 tag</Heading>
 
-      <Text>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-        voluptas nulla pariatur?
-      </Text>
+        <Text>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+          qui dolorem eum fugiat quo voluptas nulla pariatur?
+        </Text>
+      </section>
+
+      <section>
+        <Heading as="h1" fontSize="14px">
+          H1 with fontSize prop at 14px
+        </Heading>
+        <Heading as="h2" fontSize="1.2rem">
+          H2 with fontSize prop 1.2rem
+        </Heading>
+        <Heading as="h3" fontSize="1.7rem">
+          H3 with fontSize prop 1.7rem
+        </Heading>
+
+        <Text>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+          qui dolorem eum fugiat quo voluptas nulla pariatur?
+        </Text>
+      </section>
     </div>
   );
 }
--- src/components/typography/heading.tsx
+++ src/components/typography/heading.tsx
@@ -2,19 +2,23 @@ import styled from "styled-components";
 import React, { FC, ReactNode } from "react";
 import { fonts } from "../../tokens/fonts";
 
-type HeadingElementTags = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-
 export type HeadingProps = {
-  as: HeadingElementTags;
+  as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   children: JSX.Element | JSX.Element[] | ReactNode | string;
+  fontSize?: number | string;
 };
 
 const HeadingStyle = styled.div<HeadingProps>`
   font-family: ${fonts.heading};
   line-height: 1.25;
+  font-size: ${(props) => props.fontSize};
   color: "darkgrey";
 `;
 
-export const Heading: FC<HeadingProps> = ({ as, children }) => {
-  return <HeadingStyle as={as}>{children}</HeadingStyle>;
+export const Heading: FC<HeadingProps> = ({ as, children, fontSize }) => {
+  return (
+    <HeadingStyle as={as} fontSize={fontSize}>
+      {children}
+    </HeadingStyle>
+  );
 };
